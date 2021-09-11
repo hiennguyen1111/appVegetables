016f99d5d1698640ffde0e5230f66efa653fb9e1
Test FPT Poly <test_fpt_poly@fpt.edu.vn>
2021-09-11 11:39:24 +0700

    React Vegetables Project

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './Redux/reducers/rootReducer';
+import {history} from './libs/history';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router history={history}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>
+  ,
   document.getElementById('root')
+
 );
 
 // If you want to start measuring performance in your app, pass a function
